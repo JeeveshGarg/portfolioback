4496d0c8ce20c695a75edb2ac836efbc8577dcc5
Jeevesh <jeevesh.garg.min20@iitbhu.ac.in>
2021-09-05 18:31:44 +0530

    3rdgjlklkdsd

--- Back/models.py
+++ Back/models.py
@@ -8,7 +8,7 @@ from ckeditor.fields import RichTextField
 class Category(models.Model):
     name = models.CharField(max_length=100)
     color = models.CharField(max_length=100)
-    image = models.URLField(max_length=200, blank=True)
+    image = models.URLField(max_length=500, blank=True)
     description = models.TextField(blank=True)
     web= models.URLField(max_length=200, blank=True)
    
@@ -20,7 +20,7 @@ class Project(models.Model):
     description = models.TextField()
     need= models.TextField(blank=True)
     tags= models.ManyToManyField(Category, blank=True)
-    image = models.ImageField(upload_to='images/', blank=True)
+    image = models.URLField(max_length=500, blank=True)
     web= models.URLField(max_length=200, blank=True)
     github= models.CharField(max_length=200, blank=True)
     def __str__(self):
@@ -36,7 +36,7 @@ class Work(models.Model):
     name= models.CharField(max_length=100)
     description= models.TextField()
     tags= models.ManyToManyField(Value, blank=True)
-    image = models.ImageField(upload_to='images/', blank=True)
+    image = models.URLField(max_length=500, blank=True)
     web= models.URLField(max_length=200, blank=True)
     def __str__(self):
         return self.name
@@ -49,6 +49,6 @@ class Blog(models.Model):
     description = models.TextField()
     content= RichTextField(default='')
     # content_upload= RichTextUploadingField(default='')
-    image = models.ImageField(upload_to='images/', blank=True)
+    image = models.URLField(max_length=500, blank=True)
     def __str__(self):
         return self.title
