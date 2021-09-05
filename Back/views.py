d4496e03edccfe3b6fcdaddc500c0a923ba1f413
Jeevesh <jeevesh.garg.min20@iitbhu.ac.in>
2021-09-05 15:56:55 +0530

    3rdgjlk

--- Back/views.py
+++ Back/views.py
@@ -7,34 +7,34 @@ from .models import *
 from .serializers import *
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from rest_framework.status import status
 
 
 # Create your views here.
-
+@api_view(['GET'])
 def index(request):
     if request.method == 'GET':
         category=Category.objects.all()
         serializer=CategorySerializer(category,many=True)
-        return JsonResponse(serializer.data,safe=False)
+        return Response(serializer.data)
 
-
+@api_view(['GET'])
 def project(request):
     if request.method == 'GET':
         project=Project.objects.all()
         serializer=ProjectSerializer(project,many=True)
-        return JsonResponse(serializer.data,safe=False)        
+        return Response(serializer.data)        
 
-
+@api_view(['GET'])
 def blog(request):
     if request.method == 'GET':
         blog=Blog.objects.all()
         serializer=BlogSerializer(blog,many=True)
-        return JsonResponse(serializer.data,safe=False)
+        return Response(serializer.data)
 
-
+@api_view(['GET'])
 def work(request):
     if request.method == 'GET':
         work=Work.objects.all()
         serializer=WorkSerializer(work,many=True)
-        return JsonResponse(serializer.data,safe=False)        
+        return Response(serializer.data)        
