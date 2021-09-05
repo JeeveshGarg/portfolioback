2d1e9318b968411e5ea1124389f2fd855ebf93f7
Jeevesh <jeevesh.garg.min20@iitbhu.ac.in>
2021-09-05 11:23:34 +0530

    second

--- Portfolioback/settings.py
+++ Portfolioback/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+import django_heroku
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
